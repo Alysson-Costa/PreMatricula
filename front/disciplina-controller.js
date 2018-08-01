dded4e25be93dfbb0a94238d7cbb6c786ca04d35
Alysson-Costa <alysson.costa@ccc.ufcg.edu.br>
2018-08-01 14:27:08 -0300

    Update disciplina-controller.js

--- front/disciplina-controller.js
+++ front/disciplina-controller.js
@@ -2,89 +2,64 @@
   const app = angular.module('Disciplina');
   app.controller('DisciplinaCtrl', function MensagensCtrl($http, $scope) {
     $http.get('http://api-pre-matricula.herokuapp.com/disciplinas').
+      //$http.get('http://analytics.ufcg.edu.br/pre/ciencia_da_computacao_i_cg/disciplinas').
       then(function (response) {
         $scope.disciplinasCadastradas = response.data;
       });
 
 
-
-
-
-    $scope.email = function () {
-      $scope.email2 = document.getElementById('user-email').innerText;
-      console.log($scope.email2);
-     
-    }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-
-
-
-    $scope.marcarSelecionado = function (selecionado) {
-      for (const cartao of this.data.cartoes) {
-        cartao.marcado = false;
-      }
-
-      selecionado.marcado = true;
-    }
+    $scope.email = '';
 
     $scope.disciplinasSelecionadas = [];
 
-  $scope.selecionar = function (elemento) {
-    console.log($scope.email2);
-    var indice = $scope.disciplinasCadastradas.indexOf(elemento);
+    $scope.selecionar = function (elemento) {
+      console.log($scope.m);
+      var indice = $scope.disciplinasCadastradas.indexOf(elemento);
 
-    if ($scope.disciplinasSelecionadas.indexOf(elemento) < 0) {
-      $scope.disciplinasSelecionadas.push($scope.disciplinasCadastradas[indice]);
+      if ($scope.disciplinasSelecionadas.indexOf(elemento) < 0) {
+        $scope.disciplinasSelecionadas.push($scope.disciplinasCadastradas[indice]);
 
-    } else {
-      $scope.disciplinasSelecionadas.splice($scope.disciplinasSelecionadas.indexOf(elemento), 1);
+      } else {
+        $scope.disciplinasSelecionadas.splice($scope.disciplinasSelecionadas.indexOf(elemento), 1);
+      }
+    };
+
+    $scope.realizarMatricula = function () {
+      var qtdeDisciplinas = $scope.disciplinasSelecionadas.length;
+            
+      if ( qtdeDisciplinas < 4) {
+        
+        document.getElementById("modalRealizarMatricula").close();
+        alert('Número de créditos não atingido!')
+      if(qtdeDisciplinas > 9){
+        alert('Número de créditos excedido!')
+      
+      } else {
+        //realiza matricula
+      }
     }
-  };
-
-  $scope.novaTurma = function (nome, codigo, periodo) {
-
-    fetch('https://api-pre-matricula.herokuapp.com/disciplinas',
-      {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({
-          nome: nome,
-          codigo: codigo,
-          periodo: periodo,
-          carga: 60,
-          grade: "Nova"
-
-        })
-      })
-      .then(function (res) { console.log(res) })
-      .catch(function (res) { console.log(res) })
   }
+    $scope.novaTurma = function (nome, codigo, periodo) {
 
+      fetch('https://api-pre-matricula.herokuapp.com/disciplinas',
+        {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify({
+            nome: nome,
+            codigo: codigo,
+            periodo: periodo,
+            carga: 60,
+            grade: "Nova"
 
-
-
-});
-
-
+          })
+        })
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) })
+    }
+  });
   
-
-
 }());
